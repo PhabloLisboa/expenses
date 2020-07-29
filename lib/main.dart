4378f1076e61ed44ce850087f963a33d46b58e7b
Phablo Lisboa <phablolisboa01@gmail.com>
2020-07-29 11:42:05 -0300

    Transaction classe

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:expenses/Models/Transaction.dart';
 import 'package:flutter/material.dart';
 
 main() {
@@ -14,11 +15,66 @@ class ExpensesApp extends StatelessWidget {
 }
 
 class Home extends StatelessWidget {
+  final _transactions = [
+    Transaction(
+        id: 't1', title: 'Amazon Prime', value: 10, date: DateTime.now()),
+    Transaction(
+        id: 't2', title: 'Crunchyroll', value: 24.5, date: DateTime.now()),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Despesas Pessoais'),
+      ),
+      body: Column(
+        children: <Widget>[
+          Container(
+            width: double.infinity,
+            child: Card(
+              child: Text('Gráfico'),
+            ),
+          ),
+          Column(
+            children: _transactions
+                .map((e) => Card(
+                      child: Row(
+                        children: <Widget>[
+                          Container(
+                            margin: EdgeInsets.symmetric(
+                                horizontal: 15, vertical: 10),
+                            child: Text(
+                              e.value.toString(),
+                              style: TextStyle(
+                                  fontWeight: FontWeight.bold,
+                                  color: Colors.purple),
+                            ),
+                            decoration: BoxDecoration(
+                                border:
+                                    Border.all(color: Colors.purple, width: 2)),
+                            padding: EdgeInsets.all(10),
+                          ),
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: <Widget>[
+                              Text(
+                                e.title,
+                                style: TextStyle(
+                                    fontWeight: FontWeight.bold, fontSize: 16),
+                              ),
+                              Text(e.date.toString(),
+                                  style: TextStyle(
+                                    color: Colors.grey,
+                                  ))
+                            ],
+                          )
+                        ],
+                      ),
+                    ))
+                .toList(),
+          )
+        ],
       ),
     );
   }
